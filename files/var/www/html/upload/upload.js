9a1ea36a7cd09d26ee0b6c6b15d474434e1dd6bb
Bernhard Bablok <bablokb@gmx.de>
2016-11-11 14:15:59 +0100

    create previews of files ready for upload

--- files/var/www/html/upload/upload.js
+++ files/var/www/html/upload/upload.js
@@ -8,13 +8,38 @@
 //
 // ---------------------------------------------------------------------------
 
+/**
+ * Create previews of files ready for upload
+ */
+
+function createPreviews(files) {
+  for (var i = 0, f; f = files[i]; i++) {
+    console.error("createPreview",f);
+    var reader = new FileReader();
+    reader.onload = (function (theFile) {
+                       return function (e) {
+                                // create thumbs
+                                var thumb = document.createElement('img');
+                                thumb.className = 'preview';
+                                thumb.src = e.target.result;
+                                thumb.title = theFile.name;
+                                document.getElementById('previews').insertBefore(thumb,null);
+                              };
+                     })(f);
+    // read image as data url
+    reader.readAsDataURL(f);
+  }
+}
 
 /**
  * Callback for file-selection change event
  */
 
 function onChangeFileSelect(evt) {
-  console.error("onChangeFileSelect",evt);
+  // reset preview area
+  $('#previews').empty();
+
+  // loop through all files
   var files = evt.target.files; // FileList object
   var msgok = "", msgerr = "";
   for (var i = 0, f; f = files[i]; i++) {
@@ -27,12 +52,25 @@ function onChangeFileSelect(evt) {
       msgok += "<p class='msgok'>" + f.name + " (" + f.size +") ready for upload</p>";
     }
   }
+
+  // either set error message or enable upload + create thumbs
   if (msgerr.length > 0) {
     $("#msgarea").html(msgerr);
   } else {
     $("#msgarea").html(msgok);
     $("#bt_sub").prop( "disabled",false);
+    createPreviews(files);
   }
+}
+
+/**
+ * Before upload (Reset progress indicator, disable button, clear previews)
+ */
+
+function beforeSend() {
+    $('#bt_sub').prop( "disabled",true);
+    $('progress').attr({value: 0, max: 100});
+    $('#previews').empty();
 }
 
 /**
@@ -57,15 +95,6 @@ function sendError(data) {
   // console.error(data);
     var result = JSON.parse(data.responseText);
     $("#msgarea").html(result.msg);
-}
-
-/**
- * Before upload (Reset progress indicator, disable button)
- */
-
-function beforeSend() {
-    $("#bt_sub").prop( "disabled",true);
-    $('progress').attr({value: 0, max: 100});
 }
 
 /**
