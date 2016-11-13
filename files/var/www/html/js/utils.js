c8d8e90855af67a660fd8fb4ba912a0843672be0
Bernhard Bablok <bablokb@gmx.de>
2016-11-13 16:37:08 +0100

    initial entry

---
+++ files/var/www/html/js/utils.js
@@ -0,0 +1,26 @@
+// ---------------------------------------------------------------------------
+// JS support functions.
+//
+// Author: Bernhard Bablok
+// License: GPL3
+//
+// Website: https://github.com/bablokb/pi-imgtank
+//
+// ---------------------------------------------------------------------------
+
+/**
+ * Handle author info.
+ */
+
+function onAuthorClicked() {
+  alert("Copyright Bernhard Bablok, bablokb@gmx.de");
+}
+
+/**
+ * Handle license info.
+ */
+
+function onLicenseClicked() {
+  alert("Unless otherwise noted, the code of this project is realeased under the GPL3");
+}
+
